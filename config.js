787e5cfd7d03493d82670fa78bb91b37ee3a95d0
Daniel <1602268076@qq.com>
2018-12-07 18:33:32 +0800

    update

---
+++ config.js
@@ -0,0 +1,15 @@
+const config = {
+    // 本地监听的端口号
+    port: 9999,
+
+    // 数据库配置
+    database: {
+        DATABASE: 'chatRoom',
+        USERNAME: 'root',
+        PASSWORD: 'Z6m7m919930816',
+        PORT: '3306',
+        HOST: 'localhost'
+    }
+}
+
+module.exports = config
